7849acaf81e75eac4e7426975e3a70e85f67870c
ipap0 <alkaz06@yandex.ru>
2025-09-29 14:20:54 +0300

    Slices

--- strings1.py
+++ strings1.py
@@ -1,10 +1,7 @@
-# Строки, индексы
+# Срезы [start : stop - 1 : step]
 string = input('Введите любую строку: ')
 length = len(string)  # число элементов итерируемого объекта (число символов в строке)
 
-print('Строка "' + string + '" имеет длину:', length)
+print(string[::-1])  # разворот
 
-symb = string[-1]  # string[length - 1]
-
-for index in reversed(string):
-    print(index, end='')
+# Вентилятор -> лят
