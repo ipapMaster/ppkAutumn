8adcbc91837e1be420905760b5af14075809ce79
ipap0 <alkaz06@yandex.ru>
2025-09-29 09:40:02 +0300

    turtle while

--- loop1.py
+++ loop1.py
@@ -1,1 +1,12 @@
-# Циклы
+# Циклы (DRY - Don't repeat Yourself)
+import turtle
+
+count = 0
+sides = 8
+
+while count < sides:
+    turtle.forward(100)
+    turtle.right(360 / sides)
+    count += 1  # инкремент счётчика
+
+turtle.mainloop()
