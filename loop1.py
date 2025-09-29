8dd3b7caa3211ae028cab36ecbac6595ebb95771
ipap0 <alkaz06@yandex.ru>
2025-09-29 11:35:17 +0300

    Walrus Operator

--- loop1.py
+++ loop1.py
@@ -5,13 +5,14 @@
 # s - step (1 - по умолчанию)
 # for i in range(10) -> ... in range(0, 10, 1)
 
+# string = input('Введите любую строку или exit для завершения: ')
+
 loop = True
 
 while loop:
-    string = input('Введите любую строку: ')
-    if not string:
-        loop = False
+    string = input('Введите любую строку или exit для завершения: ')
+    if string == 'exit':
+        loop = False  # 0
+    print('Вы ввели: ' + string)
 
-print('Вы ничего не ввели. Программа завершена')
-
-
+print('Вы ввели: exit. Программа завершена.')
