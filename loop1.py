da0c2802d73963ef2c0c404b92aca559d412c0b8
ipap0 <alkaz06@yandex.ru>
2025-09-29 10:24:24 +0300

    turtle while2

--- loop1.py
+++ loop1.py
@@ -1,12 +1,30 @@
 # Циклы (DRY - Don't repeat Yourself)
+# for count in range(s, s, s)
+# s - start (0 - по умолчанию)
+# s - stop (не включая)
+# s - step (1 - по умолчанию)
+# for i in range(10) -> ... in range(0, 10, 1)
 import turtle
 
-count = 0
-sides = 8
+count = 0  # Число фигур
+sides = 0
+N = 10
+M = 5
 
-while count < sides:
-    turtle.forward(100)
-    turtle.right(360 / sides)
-    count += 1  # инкремент счётчика
+turtle.speed(0)  # мгновенная скорость
+
+# переносим "черепаху" на 50 вверх и на 30 влево
+turtle.penup()
+turtle.goto(-30, 50)
+turtle.pendown()
+
+while count < N:  # 8
+    while sides < M:  # 4
+        turtle.forward(100)
+        turtle.right(360 / M)
+        sides += 1
+    turtle.right(360 / N)
+    count += 1
+    sides = 0
 
 turtle.mainloop()
