1eed782cb8534abe5ef2b030e03f41f8319e7c1b
ipap0 <alkaz06@yandex.ru>
2025-09-29 10:47:35 +0300

    first-second

--- loop1.py
+++ loop1.py
@@ -4,27 +4,12 @@
 # s - stop (не включая)
 # s - step (1 - по умолчанию)
 # for i in range(10) -> ... in range(0, 10, 1)
-import turtle
 
-count = 0  # Число фигур
-sides = 0
 N = 10
-M = 5
 
-turtle.speed(0)  # мгновенная скорость
+for i in range(1, N + 1):
+    if i % 2 == 0:
+        print('Второй')
+    else:
+        print('Первый')
 
-# переносим "черепаху" на 50 вверх и на 30 влево
-turtle.penup()
-turtle.goto(-30, 50)
-turtle.pendown()
-
-while count < N:  # 8
-    while sides < M:  # 4
-        turtle.forward(100)
-        turtle.right(360 / M)
-        sides += 1
-    turtle.right(360 / N)
-    count += 1
-    sides = 0
-
-turtle.mainloop()
