ef554c9f7fa88c575786ebb900833ea61900e6a2
ipap0 <alkaz06@yandex.ru>
2025-09-29 10:51:46 +0300

    wihout break

--- loop1.py
+++ loop1.py
@@ -5,11 +5,11 @@
 # s - step (1 - по умолчанию)
 # for i in range(10) -> ... in range(0, 10, 1)
 
-N = 10
+string = input('Введите любую строку: ')
 
-for i in range(1, N + 1):
-    if i % 2 == 0:
-        print('Второй')
-    else:
-        print('Первый')
+while string != '':
+    string = input('Введите любую строку: ')
+
+print('Вы ничего не ввели. Программа завершена')
+
 
