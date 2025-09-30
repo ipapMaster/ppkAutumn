883ea1ee07307d77b838078c08c25272971b43ee
ipap0 <alkaz06@yandex.ru>
2025-09-30 11:59:34 +0300

    List Sort

--- list3.py
+++ list3.py
@@ -1,13 +1,2 @@
 # Список (имитация стека)
-stack = []
 
-for i in range(5):
-    stack.append(f'Простыня_{i + 1}')
-
-print('Наполняем стек:')
-for item in stack:
-    print(item)
-
-print('Удаляем стек:')
-while stack:  # пока список не пустой
-    print(stack.pop())
