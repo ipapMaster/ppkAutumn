964fb2fc51cf9fb2efba2eb10b87c41206b326bb
ipap0 <alkaz06@yandex.ru>
2025-10-07 13:22:01 +0300

    Own Modeles and Packages

---
+++ my_package/__init__.py
@@ -0,0 +1,2 @@
+from .module_1 import hello
+from .module_2 import goodbye
---
+++ own_module2.py
@@ -0,0 +1,12 @@
+# Собственные модули
+# Их создание и подключение
+# Подключаем нужную функцию из пакета модуля my_package
+from my_package import hello, goodbye
+
+hello()
+goodbye()
+
+
+
+
+
