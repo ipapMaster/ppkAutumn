0aca38adcd1904f150a90d2dfd1c51b26484c61f
ipap0 <alkaz06@yandex.ru>
2025-10-07 10:07:13 +0300

    Word Doc Creating

--- modules6.py
+++ modules6.py
@@ -6,20 +6,25 @@
 from PIL import Image, ImageDraw
 
 # создали холст
-new_image = Image.new('RGB', (600, 400), (180, 180, 180))
+new_image = Image.new('RGB', (600, 400), (255, 255, 255))
 
 # создаём объект для рисования и указываем на чём рисовать
 draw = ImageDraw.Draw(new_image)
 
+# Рисуем два прямоугольника
+draw.rectangle((0, 0, 599, 132), (245, 245, 245))
+draw.rectangle((0, 133, 599, 266), (0, 0, 255))
+draw.rectangle((0, 267, 599, 400), (255, 0, 0))
+
 # Рисуем прямоугольник по контуру толщиной 3 пикселя
-draw.rectangle((0, 0, 599, 399), outline=(0, 0, 255), width=3)
+# draw.rectangle((0, 0, 599, 399), outline=(0, 0, 255), width=3)
 # рисуем линию, разделяющую холст пополам
 # draw.line((299, 0, 299, 399), fill=(0, 0, 255), width=3)
 # рисуем две диагонали
-draw.line((0, 0, 599, 399), fill=(0, 0, 255), width=3)
-draw.line((599, 0, 0, 399), fill=(0, 0, 255), width=3)
+# draw.line((0, 0, 599, 399), fill=(0, 0, 255), width=3)
+# draw.line((599, 0, 0, 399), fill=(0, 0, 255), width=3)
 
-new_image.save('images/canvas.jpg')
+new_image.save('images/flag.jpg')
 
 # оригинальное изображение
 # image = Image.open('images/python.jpg')
