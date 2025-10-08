d9c7e3f29f9cf34bccf763455e6797a62d799b6b
ipap0 <alkaz06@yandex.ru>
2025-10-08 11:41:58 +0300

    OOP Polymorphism2

---
+++ oop5.py
@@ -0,0 +1,34 @@
+# Основные понятия в ООП
+# Полиморфизм (polymorphism - продолжение)
+# Свойство кода работать с разными типами данных
+# Функция isinstance(объект, класс) -> True или False
+
+# Student, Employee, Person
+class Student:
+    def __init__(self, u=''):
+        self.university = u
+
+
+class Employee:
+    def __init__(self, c=''):
+        self.company = c
+
+
+class Person:
+    def __init__(self, n=''):
+        self.name = n
+
+
+s = Student('Техноложка')
+e = Employee('Авангард')
+p = Person('Дима')
+
+persons = [s, e, p]
+
+for person in persons:
+    if isinstance(person, Student):
+        print(person.university)
+    elif isinstance(person, Employee):
+        print(person.company)
+    else:
+        print(person.name)
