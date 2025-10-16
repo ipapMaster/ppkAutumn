e690f11bf311d17b5efcbdeba5e2f763f5b09abf
ipap0 <alkaz06@yandex.ru>
2025-10-16 12:52:15 +0300

    Flask WTF-Forms

---
+++ loginform.py
@@ -0,0 +1,16 @@
+# loginform.py - класс для формы авторизации
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired, Email, Length
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин:',
+                           validators=[DataRequired('Заполните Ваше имя'),
+                                       Email('Некорректный E-mail адрес')])
+    password = PasswordField('Пароль:',
+                             validators=[DataRequired('Пароль обязателен'),
+                                         Length(min=8,
+                                                message='Пароль слишком короткий')])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
