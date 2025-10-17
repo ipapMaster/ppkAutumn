cfc74737547446b2ffe7531f0040dc9c9cc90e41
ipap0 <alkaz06@yandex.ru>
2025-10-17 14:59:59 +0300

    Flask SQLAlchemy Authentication

--- forms/loginform.py
+++ forms/loginform.py
@@ -5,12 +5,12 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Логин:',
+    email = StringField('Логин:',
                            validators=[DataRequired('Заполните Ваше имя'),
                                        Email('Некорректный E-mail адрес')])
     password = PasswordField('Пароль:',
                              validators=[DataRequired('Пароль обязателен'),
-                                         Length(min=8,
+                                         Length(min=3,
                                                 message='Пароль слишком короткий')])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
