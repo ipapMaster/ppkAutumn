a29ea20b84fa2065e88a844f80d00ae5850a5247
ipap0 <alkaz06@yandex.ru>
2025-10-22 11:13:45 +0300

    Flask API One News

--- data/news_api.py
+++ data/news_api.py
@@ -1,6 +1,6 @@
 # Blueprint ("синька") для API
 import flask
-from flask import jsonify
+from flask import jsonify, make_response
 from . import db_session
 from .news import News
 
@@ -21,5 +21,19 @@ def get_news():
             'news':
                 [item.to_dict(only=('title', 'content', 'user.name'))
                  for item in news]
+        }
+    )
+
+
+# Получить одну новость по ID
+@blueprint.route('/api/news/<int:news_id>', methods=['GET'])
+def get_one_news(news_id):
+    db_sess = db_session.create_session()
+    news = db_sess.get(News, news_id)
+    if not news:
+        return make_response(jsonify({'error': f'Новость с ID: {news_id} не найдена.'}))
+    return jsonify(
+        {
+            'news': news.to_dict(only=('title', 'content', 'user_id', 'is_private'))
         }
     )
